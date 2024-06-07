f53ba0289c9d12105ca25c93aefed018de3c957a
NiPoltapova <120168189+NiPoltapova@users.noreply.github.com>
2024-06-07 14:13:23 +0300

    Delete practice directory

--- practice/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-
-create table if not exists ResultOfCalculation
-(
-    id integer auto_increment,
-    expression varchar(200) not null unique,
-    result integer not null,
-    CONSTRAINT idMemory PRIMARY KEY (id)
-);
